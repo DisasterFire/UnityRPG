f5bcfd5f8035a508f8d8a329d25f3b90232fa054
Alex Presser <alexander.presser@mymail.champlain.edu>
2022-11-03 12:38:27 -0400

    Updating Combat, adding in world interaction

---
+++ RPG/Assets/Scripts/BattleScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public enum CombatState { START, PLAYER_TURN, ASSAILANT_TURN, VICTORY, DEFEAT}
+
+public class BattleScript : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject assailant;
+
+    public TMP_Text assailantDialogue;
+
+    public Transform playerStation;
+    public Transform assailantStation;
+
+    Unit playerUnit;
+    Unit assailantUnit;
+
+    public CombatState state;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = CombatState.START;
+        CreateCombat();
+    }
+
+    void CreateCombat()
+    {
+        GameObject playerObject = Instantiate(player, playerStation);
+        playerUnit = playerObject.GetComponent<Unit>();
+
+
+        GameObject assailantObject = Instantiate(assailant, assailantStation);
+        assailantUnit = assailantObject.GetComponent<Unit>();
+
+        assailantDialogue.text = assailantUnit.entityName + ", The Chemist";
+
+    }
+
+}
---
+++ RPG/Assets/Scripts/Unit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string entityName;
+    public int entityLevel;
+
+    public int damage;
+
+    public int maxHealth;
+    public int currentHealth;
+}
