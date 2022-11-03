c56f6441dcd28eda2a063223995f54052afc03cc
Alex Presser <alexander.presser@mymail.champlain.edu>
2022-11-03 12:47:28 -0400

    Initializing combat entrance

--- RPG/Assets/Scripts/EnemyInteraction.cs
+++ RPG/Assets/Scripts/EnemyInteraction.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class EnemyInteraction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject player;
+    public Transform playerPos;
+    public GameObject assailant;
+
+    public string combatScreen;
 
     // Update is called once per frame
     void Update()
     {
-        
+      
     }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log("Entered");
+        EnterCombat();
+    }
+
+    void EnterCombat()
+    {
+        if(Input.GetKeyDown("e"))
+        {
+            SceneManager.LoadScene(combatScreen);
+        }
+    }
+
 }
