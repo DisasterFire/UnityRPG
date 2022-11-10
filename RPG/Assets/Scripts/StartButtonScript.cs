9937d93d90f068ab714ec63ed5ff34c94d48ef9f
Alex Presser <alexander.presser@mymail.champlain.edu>
2022-11-10 11:50:49 -0500

    Adding in a main menu functionality for the game

---
+++ RPG/Assets/Scripts/StartButtonScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButtonScript : MonoBehaviour
+{
+    public string gameLevel;
+    
+    public void OnButtonPress()
+    {
+        Debug.Log("Button Pressed");
+        SceneManager.LoadScene(gameLevel);
+    }
+
+}
