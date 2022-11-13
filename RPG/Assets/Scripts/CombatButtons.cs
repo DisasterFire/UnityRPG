4b265b03eb721d22c47b4aa17940f84e10500c91
Alex Presser <alexander.presser@mymail.champlain.edu>
2022-11-13 16:35:16 -0500

    adding in a new script to change up how combat is played out in game

--- RPG/Assets/Scripts/CombatButtons.cs
+++ RPG/Assets/Scripts/CombatButtons.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class CombatButtons : MonoBehaviour
 {
+    public bool standard, darkness, divinity;
+    public string userInput;
+
     public void ViewEnemyStats()
     {
 
     }
 
-    public void Abilities()
+    public void StandardAbilities()
     {
+        standard = true;
+        darkness = false;
+        divinity = false;
+    }
 
+    public void DarknessAbilities()
+    {
+        standard = false;
+        darkness = true;
+        divinity = false;
+    }
+
+    public void DivinityAbilities()
+    {
+        standard = false;
+        darkness = false;
+        divinity = true;
     }
 }
