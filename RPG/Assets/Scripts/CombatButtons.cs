ed2f1a060e7474e10e4768c997977bf0308b19fd
Alex Presser <alexander.presser@mymail.champlain.edu>
2022-11-14 12:43:40 -0500

    Touching up combat/ability usage. Will work on adding in a turn system, but just getting the basics working

--- RPG/Assets/Scripts/CombatButtons.cs
+++ RPG/Assets/Scripts/CombatButtons.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CombatButtons : MonoBehaviour
 {
     public bool standard, darkness, divinity;
-    public int maxEnemyHP = 20;
+    public int maxEnemyHP;
     public int enemyHP;
+    public int maxPlayerAP;
+    public int playerAP;
+    public bool outOfAP = false;
+    public string winScreen;
+    bool sInitialized = false;
+    bool dInitialized = false;
+
+    public Button darkButton;
+    public Button divineButton;
 
     public void ViewEnemyStats()
     {
@@ -18,7 +29,11 @@ public class CombatButtons : MonoBehaviour
         standard = true;
         darkness = false;
         divinity = false;
-        enemyHP = maxEnemyHP;
+        if(!sInitialized)
+        {
+            enemyHP = maxEnemyHP;
+            sInitialized = true;
+        }
     }
 
     public void DarknessAbilities()
@@ -26,6 +41,19 @@ public class CombatButtons : MonoBehaviour
         standard = false;
         darkness = true;
         divinity = false;
+
+        if (!sInitialized)
+        {
+            enemyHP = maxEnemyHP;
+            sInitialized = true;
+        }
+
+        if (!dInitialized)
+        {
+            playerAP = maxPlayerAP;
+            dInitialized = true;
+        }
+
     }
 
     public void DivinityAbilities()
@@ -33,10 +61,62 @@ public class CombatButtons : MonoBehaviour
         standard = false;
         darkness = false;
         divinity = true;
+
+        if (!sInitialized)
+        {
+            enemyHP = maxEnemyHP;
+            sInitialized = true;
+        }
+
+        if (!dInitialized)
+        {
+            playerAP = maxPlayerAP;
+            dInitialized = true;
+        }
     }
 
     public void Attack()
+    {   
+        enemyHP -= 2;
+        if(enemyHP <= 0)
+        {
+            WinGame();
+        }
+    }
+
+    public void DarkAttack()
     {
-        enemyHP -= 1;
+        enemyHP -= 5;
+        playerAP -= 2;
+        if (enemyHP <= 0)
+        {
+            WinGame();
+        }
+
+        if(playerAP <= 0)
+        {
+           darkButton.GetComponent<Button>().interactable = false;
+        }
+
+    }
+
+    public void DivineAttack()
+    {
+        enemyHP -= 10;
+        playerAP -= 3;
+        if (enemyHP <= 0)
+        {
+            WinGame();
+        }
+
+        if (playerAP <= 0)
+        {
+            outOfAP = true;
+        }
+    }
+
+    void WinGame()
+    {
+        SceneManager.LoadScene(winScreen);
     }
 }
